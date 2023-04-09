00100016a33e01ea4e9903fa67eb6bff5038fa2b
jgsimard <jeangab23@gmail.com>
2023-04-09 18:12:14 -0400

    remove python for the moment for faster build

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
-use localization::{
-    extended_kalman_filter::{ExtendedKalmanFilter, ExtendedKalmanFilterKnownCorrespondences},
-    particle_filter::ParticleFilterKnownCorrespondences,
-    unscented_kalman_filter::UnscentedKalmanFilter,
-};
-use nalgebra::Dyn;
-use pyo3::prelude::*;
+// use localization::{
+//     extended_kalman_filter::{ExtendedKalmanFilter, ExtendedKalmanFilterKnownCorrespondences},
+//     particle_filter::ParticleFilterKnownCorrespondences,
+//     unscented_kalman_filter::UnscentedKalmanFilter,
+// };
+// use nalgebra::Dyn;
+// use pyo3::prelude::*;
 
 pub mod data;
 pub mod localization;
@@ -12,46 +12,46 @@ pub mod mapping;
 pub mod models;
 pub mod utils;
 
-#[pyclass]
-struct Ekf {
-    _ekf: ExtendedKalmanFilter<f64, Dyn, Dyn, Dyn>,
-}
-// impl EKF {
-//     #[new]
-//     fn new(r : ) {
-
-//     }
+// #[pyclass]
+// struct Ekf {
+//     _ekf: ExtendedKalmanFilter<f64, Dyn, Dyn, Dyn>,
 // }
-#[pyclass]
-struct Ekfkc {
-    _ekf: ExtendedKalmanFilterKnownCorrespondences<f64, Dyn, Dyn, Dyn>,
-}
+// // impl EKF {
+// //     #[new]
+// //     fn new(r : ) {
 
-#[pyclass]
-struct Ukf {
-    _ukf: UnscentedKalmanFilter<f64, Dyn, Dyn, Dyn>,
-}
+// //     }
+// // }
+// #[pyclass]
+// struct Ekfkc {
+//     _ekf: ExtendedKalmanFilterKnownCorrespondences<f64, Dyn, Dyn, Dyn>,
+// }
 
-#[pyclass]
-struct Pf {
-    _pf: ParticleFilterKnownCorrespondences<f64, Dyn, Dyn, Dyn>,
-}
+// #[pyclass]
+// struct Ukf {
+//     _ukf: UnscentedKalmanFilter<f64, Dyn, Dyn, Dyn>,
+// }
 
-// python port // must include pyo3 to make this work
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
+// #[pyclass]
+// struct Pf {
+//     _pf: ParticleFilterKnownCorrespondences<f64, Dyn, Dyn, Dyn>,
+// }
 
-/// A Python module implemented in Rust.
-#[pymodule]
-fn robotics(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    m.add_class::<Ekf>()?;
-    m.add_class::<Ekfkc>()?;
-    m.add_class::<Ukf>()?;
-    m.add_class::<Pf>()?;
+// // python port // must include pyo3 to make this work
+// /// Formats the sum of two numbers as string.
+// #[pyfunction]
+// fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+//     Ok((a + b).to_string())
+// }
 
-    Ok(())
-}
+// /// A Python module implemented in Rust.
+// #[pymodule]
+// fn robotics(_py: Python, m: &PyModule) -> PyResult<()> {
+//     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+//     m.add_class::<Ekf>()?;
+//     m.add_class::<Ekfkc>()?;
+//     m.add_class::<Ukf>()?;
+//     m.add_class::<Pf>()?;
+
+//     Ok(())
+// }
