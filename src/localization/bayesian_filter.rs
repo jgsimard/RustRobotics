f73e863d508d8fadcd1de4e61a5795b90d3c45e3
jgsimard <jeangab23@gmail.com>
2023-04-12 15:08:41 -0400

    simplify necessary allocators

--- src/localization/bayesian_filter.rs
+++ src/localization/bayesian_filter.rs
@@ -1,29 +1,12 @@
-use nalgebra::{allocator::Allocator, Const, DefaultAllocator, Dim, OVector, RealField};
+use nalgebra::{allocator::Allocator, DefaultAllocator, Dim, OVector, RealField};
 
 use crate::utils::state::GaussianState;
 
 pub trait BayesianFilter<T: RealField, S: Dim, Z: Dim, U: Dim>
 where
-    DefaultAllocator: Allocator<T, S>
-        + Allocator<T, U>
-        + Allocator<T, Z>
-        + Allocator<T, S, S>
-        + Allocator<T, Z, Z>
-        + Allocator<T, Z, S>
-        + Allocator<T, S, U>
-        + Allocator<T, U, U>
-        + Allocator<T, S, Z>
-        + Allocator<T, Const<1>, S>
-        + Allocator<T, Const<1>, Z>,
+    DefaultAllocator: Allocator<T, S> + Allocator<T, U> + Allocator<T, Z> + Allocator<T, S, S>,
 {
-    fn update_estimate(
-        &mut self,
-        // estimate: &GaussianState<T, S>,
-        u: &OVector<T, U>,
-        z: &OVector<T, Z>,
-        dt: T,
-    );
-    // ) -> GaussianState<T, S>;
+    fn update_estimate(&mut self, u: &OVector<T, U>, z: &OVector<T, Z>, dt: T);
 
     fn gaussian_estimate(&self) -> GaussianState<T, S>;
 }
